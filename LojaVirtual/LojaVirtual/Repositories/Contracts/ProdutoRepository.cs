8abef90f6135fd7c216846939a0927f235c0c19f
samuel vicenca <samuelvicenca@hotmail.com>
2020-03-03 14:53:09 -0300

    Chack-in

--- LojaVirtual/LojaVirtual/Repositories/Contracts/ProdutoRepository.cs
+++ LojaVirtual/LojaVirtual/Repositories/Contracts/ProdutoRepository.cs
@@ -1,11 +1,60 @@
-﻿using System;
+﻿using LojaVirtual.Database;
+using LojaVirtual.Models;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using X.PagedList;
 
 namespace LojaVirtual.Repositories.Contracts
 {
-    public class ProdutoRepository
+    public class ProdutoRepository : IProdutoRepository
     {
+        private IConfiguration _conf;
+        private LojaVirtualContext _banco;
+
+        public ProdutoRepository(LojaVirtualContext banco, IConfiguration conf)
+        {
+            _banco = banco;
+            _conf = conf;
+        }
+
+        public void Atualizar(Produto produto)
+        {
+            _banco.Update(produto);
+            _banco.SaveChanges();
+        }
+
+        public void Cadastrar(Produto produto)
+        {
+            _banco.Add(produto);
+            _banco.SaveChanges();
+        }
+
+        public void Excluir(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Produto ObterProduto(int Id)
+        {
+            return _banco.Produtos.Find(Id);
+        }      
+        public IPagedList<Produto> ObterTodosProdutos(int? pagina, string pesquisa)
+        {
+            int RegistroPorPagina = _conf.GetValue<int>("RegistroPorPagina");
+
+            int NumeroPagina = pagina ?? 1;
+
+            var bancoProduto = _banco.Produtos.AsQueryable();
+
+            if (!string.IsNullOrEmpty(pesquisa))
+            {
+                bancoProduto = bancoProduto.Where(a => a.Nome.Contains(pesquisa.Trim()));
+            }
+
+            return _banco.Produtos.ToPagedList(NumeroPagina, RegistroPorPagina);
+        }     
     }
 }
